104d7136b61d278e730fa7084f954836175a68ca
Ralf Pannemans <ralf.pannemans@sap.com>
2019-02-04 21:06:38 +0100

    First version for cf

--- app/app.py
+++ app/app.py
@@ -1,26 +1,34 @@
 import os
+import sys
 
 from flask import Flask, render_template, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-
-database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
+database_uri = 'postgresql://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbname}'.format(
+    dbuser=os.environ.get('DBUSER', "user"),
+    dbpass=os.environ.get('DBPASS', "pass"),
+    dbhost=os.environ.get('DBHOST', "dbhost"),
+    dbport=os.environ.get('DBPORT', "5678"),
+    dbname=os.environ.get('DBNAME', "dbname")
 )
 
 app = Flask(__name__)
+port = int(os.getenv('PORT', '8000'))
+
 app.config.update(
     SQLALCHEMY_DATABASE_URI=database_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
 )
 
+print("Connecting to:", database_uri)
 # initialize the database connection
 db = SQLAlchemy(app)
+if not db:
+    print("Could not connect!!!")
+    sys.exit(1)
 
+print("Migrating...")
 # initialize database migration management
 migrate = Migrate(app, db)
 
@@ -30,6 +38,11 @@ def view_registered_guests():
     from models import Guest
     guests = Guest.query.all()
     return render_template('guest_list.html', guests=guests)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 'OK'
 
 
 @app.route('/register', methods=['GET'])
@@ -49,3 +62,8 @@ def register_guest():
 
     return render_template(
         'guest_confirmation.html', name=name, email=email)
+
+# start the app
+if __name__ == '__main__':
+    print('listening on port', port)
+    app.run(host='0.0.0.0', port=port)
